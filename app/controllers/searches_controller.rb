d646c1747a4d0c8eca504719730a8869471fbfaf
Karel Minarik <karmi@karmi.cz>
2012-08-25 11:50:19 +0200

    [SEARCH] Changed the search definition to a DSL-based syntax, added sorting by downloads
    * Used the DSL notation for defining the search, using a text prefix query on the "name" field,
      basically replicating the simple query string search with wildcards with a more performant version
    
    * Added sorting of the results by downloads (descending)
    
    * Added a Cucumber scenario for showing the more downloaded gems higher in search results
    
    * Added supporting Cucumber code: a "I have a gem with downloads" and "I see these search results" step definitions

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,17 @@
 class SearchesController < ApplicationController
 
   def show
-    Tire.configure { logger STDERR } if Rails.env.development?
-
     if params[:query]
-      @gems = Rubygem.tire.search "name:(*#{params[:query]}*) AND indexed:true",
-                                  :page     => params[:page],
+      @gems = Rubygem.tire.search :page     => params[:page],
                                   :per_page => Rubygem.per_page,
-                                  :load     => {:include => 'versions'}
+                                  :load     => {:include => 'versions'} do |search|
+        search.query  { |q| q.text 'name', params[:query], :type => 'phrase_prefix', :operator => 'and' }
+        search.filter :term, :indexed => true
+        search.sort   { by :downloads, 'desc' }
+
+        STDOUT.puts search.to_curl if Rails.env.development?
+      end
+
       @exact_match = Rubygem.name_is(params[:query]).with_versions.first
     end
   end
