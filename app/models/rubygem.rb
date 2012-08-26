fa7d1cd5a06220d13aa61a61018f227378b19341
Karel Minarik <karmi@karmi.cz>
2012-08-26 14:19:27 +0200

    [SEARCH] Added a more complex mapping definition and serialization for the Rubygem model
    * Previously, only the `name`, `downloads` and `indexed` attributes were indexed,
      replicating the functionality of the current search feature.
    
    * The `to_indexed_json` method was removed, relying on Tire's JSON serialization routines
      based on the model mapping definition.
    
    * The `summary`, `description` and `author` gem properties were added, allowing much better
      search results _recall_, ie. allowing search in these fields as well and widening the search “net”.
    
    * A gem which mentions "sinatra" in it's summary/description will now be matched (with a lower score):
      <http://localhost:3000/search?query=sinatra>.
    
    * A gem written by Florian Hanke will now be matched: http://localhost:3000/search?query=florian+hanke
    
    * The `version` gem property was added, allowing searches based on gem versions, for instance looking
      for Sinatra 1.3.2: <http://localhost:3000/search?query=name:sinatra+version:1.3.2>. For improved
      usability, the link from the result listing _should_ lead to the relevant version page,
      ie. http://localhost:3000/gems/sinatra/versions/1.3.2, not the last version.
    
    * The `depends` and `uses` gem properties were added, which index runtime gem dependencies and all
      gem dependencies, respectively. It allows searches such as <http://localhost:3000/search?query=depends:rack>
      (for gems with depend on rack) or http://localhost:3000/search?query=uses:rack (for gems which use rack in
      one way or other).
    
    * The `created_at` and `updated_at` gem properties were added, which allow to search gems updated in a specific
      period, for instance on August, 26th: <http://localhost:3000/search?query=updated_at:[2012-08-26+TO+2012-08-27]>
    
    * The `author`, `created_at` and `updated_at` also allow for a _faceted navigation_ in the future, ie. searching
      for certain gem while restricting the result to certain author or time.
    
    * These properties also allow for computing statistics on the Rubygem collection, such as displaying authors
      with most gems, or authors of the most downloaded gems, etc.
    
    You have to reindex the elasticsearch index, to pick up the new mapping and index records properly:
    
        $ bundle exec rake environment tire:import CLASS='Rubygem' FORCE=1
    
    See the following resources for information on previous efforts to implement a better Rubygems.org search:
    
    * https://groups.google.com/forum/#!topic/gemcutter/xIzyTmFdXVo/discussion
    * http://florianhanke.com/blog/2011/02/13/a-better-rubygems-search.html
    * http://blog.websolr.com/post/3505941785/rubygems-search-upgrade-2
    * http://blog.websolr.com/post/3505969969/rubygems-search-upgrade-3

--- app/models/rubygem.rb
+++ app/models/rubygem.rb
@@ -34,11 +34,26 @@ class Rubygem < ActiveRecord::Base
       mapping do
         indexes :name,      :type => 'multi_field',
                             :fields => {
-                              :name => { :type => 'string', :analyzer => 'rubygem' },
-                              :raw  => { :type => 'string', :index    => 'not_analyzed' }
+                              :name => { :type => 'string', :analyzer => 'rubygem',      :boost => 10.0 },
+                              :raw  => { :type => 'string', :index    => 'not_analyzed', :boost => 10.0 }
                             }
-        indexes :indexed,   :type => 'boolean'
-        indexes :downloads, :type => 'integer'
+        indexes :indexed,   :type => 'boolean', :include_in_all => false, :as => proc { versions.any?(&:indexed?) }
+        indexes :downloads, :type => 'integer', :include_in_all => false
+
+        indexes :summary,     :analyzer => 'english', :as => proc { versions.most_recent.try(:summary) }
+        indexes :description, :analyzer => 'english', :as => proc { versions.most_recent.try(:description) }
+        indexes :author,      :as => proc { versions.most_recent.try(:authors).try(:split, /\s*,\s*/) }
+
+        indexes :version,     :analyzer => 'keyword', :as => proc { versions.map(&:number) },
+                              :include_in_all => false
+
+        indexes :uses,        :as => proc { versions.most_recent.dependencies.map(&:name) if versions.most_recent },
+                              :include_in_all => false
+        indexes :depends,     :as => proc { versions.most_recent.dependencies.runtime.map(&:name) if versions.most_recent },
+                              :include_in_all => false
+
+        indexes :created_at,  :type => 'date', :include_in_all => false
+        indexes :updated_at,  :type => 'date', :include_in_all => false
       end
     end
   end
@@ -178,10 +193,6 @@ class Rubygem < ActiveRecord::Base
 
   def to_param
     name
-  end
-
-  def to_indexed_json
-    MultiJson.dump :name => name, :indexed => versions.any?(&:indexed?), :downloads => downloads
   end
 
   def with_downloads
