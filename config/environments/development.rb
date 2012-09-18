c124e63dce3f97f49cb3037a70a00817dc78bf3d
Karel Minarik <karmi@karmi.cz>
2012-09-18 22:44:34 +0200

    [DEPLOY] Make `development` mode more turbo for deployment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,11 @@
 Gemcutter::Application.configure do
-  config.cache_classes = false
-  config.whiny_nils = true
+  config.cache_classes = true
 
-  config.consider_all_requests_local = true
-  config.action_controller.perform_caching             = false
+  config.consider_all_requests_local = false
+  config.action_controller.perform_caching = true
+  config.action_dispatch.x_sendfile_header = "X-Sendfile"
   config.active_support.deprecation = :log
   config.action_mailer.raise_delivery_errors = false
+  config.serve_static_assets = false
+  config.i18n.fallbacks = true
 end
